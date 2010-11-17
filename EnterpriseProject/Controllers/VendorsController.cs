c16a8a60bc9d08bfd419ef07c59dea2f0e90f6de
Jonathan Meaney <jon.meaney@gmail.com>
2010-11-17 20:33:50 +0000

    eh

---
+++ EnterpriseProject/Controllers/VendorsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EnterpriseProject.Models;
+
+namespace EnterpriseProject.Controllers
+{
+    public class VendorsController : Controller
+    {
+        //
+        // GET: /Vendors/
+        FeedMeEntities db = new FeedMeEntities();
+        
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        //
+        // GET: /Vendors/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Vendors/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Vendors/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Vendors/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Vendors/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Vendors/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Vendors/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EnterpriseProject/Models/VendorsRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnterpriseProject.Models
+{
+    public class VendorsRepository
+    {
+        FeedMeEntities entities = new FeedMeEntities();
+
+        public IQueryable<Vendor> FindAllVendors()
+        {
+            return entities.Vendors;
+        }
+
+        public Vendor GetVendor(System.Guid guid)
+        {
+            return entities.Vendors.FirstOrDefault(i => i.VendorId == guid);
+        }
+
+        public void add(Vendor vendor)
+        {
+            entities.Vendors.AddObject(vendor);
+        }
+
+        public void delete(Vendor vendor)
+        {
+            entities.Vendors.DeleteObject(vendor);
+        }
+
+        public void save()
+        {
+            entities.SaveChanges();
+        }
+    }
+}
