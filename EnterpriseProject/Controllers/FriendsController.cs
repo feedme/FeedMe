63120504cf3a604a112c59729b922dfd3406abdf
the-gar <gary.christopher@rocketmail.com>
2010-11-24 19:17:56 +0000

    Add structure for FriendsController actions

---
+++ EnterpriseProject/Controllers/FriendsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnterpriseProject.Controllers
+{
+    public class FriendsController : Controller
+    {
+        //
+        // GET: /Friends/
+        // Retrieve a list of all users friends
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // List all users that are not friends
+        public ActionResult Find()
+        {
+            return View();
+        }
+
+        //Add a friendship
+        [HttpPost]
+        public ActionResult Add(Guid UserId)
+        {
+            return View();
+        }
+
+        //Destroy friendship
+        [HttpPost]
+        public ActionResult Destroy(Guid UserId)
+        {
+            return View();
+        }
+
+    }
+}
