b36a32cfb14610c1268a126b061d67d9ee303066
the-gar <gary.christopher@rocketmail.com>
2010-12-18 16:01:57 +0000

    Add AccountController test - incomplete

---
+++ EnterpriseProject.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EnterpriseProject;
+using EnterpriseProject.Controllers;
+
+namespace EnterpriseProject.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void LogOn()
+        {
+            AccountController controller = new AccountController();
+            ViewResult result = controller.LogOn() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        /*[TestMethod]
+        public void Register()
+        {
+            AccountController controller = new AccountController();
+            ViewResult result = controller.Register() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ChangePassword()
+        {
+            AccountController controller = new AccountController();
+            ViewResult result = controller.ChangePassword() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+         */
+    }
+}
